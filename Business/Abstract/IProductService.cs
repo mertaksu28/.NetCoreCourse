449e02d49c81e066b154f8cf5149a31bffaabd40
mertaksu28 <mertaksu28@outlook.com.tr>
2021-10-18 11:31:13 +0300

    BusinessLayerAndDependencyInjection

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        Product GetById(int id);
+        List<Product> GetList();
+        List<Product> GetListByProduct(int categoryId);
+        void Add(Product product);
+        void Delete(Product product);
+        void Update(Product product);
+    }
+}
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,52 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+
+        public List<Product> GetList()
+        {
+            return _productDal.GetList();
+        }
+
+        public List<Product> GetListByProduct(int categoryId)
+        {
+            return _productDal.GetList(p => p.CategoryId == categoryId);
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}
